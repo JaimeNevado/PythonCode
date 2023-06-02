db7484adc55c4fdd99ffdee4b45b6d178370f984
Jaime Nevado Farfán <djamperiio@gmail.com>
2023-06-02 11:03:28 +0200

    Primera clase

---
+++ PrimeraClase/holaMundo.py
@@ -0,0 +1,46 @@
+# Escribe por pantalla el texto Hola Mundo.
+
+
+# He declarado la variable edad
+
+'''
+Hola soy un comentario
+que tal
+'''
+#nombrePersona = input("Escribe tu nombre: ", )
+#edad = input("Escriba su edad: ")
+altura = 2
+altura2 = 3
+
+#Suma +
+#Resta -
+#Multiplicacion *
+#Division /
+
+print("La suma de las dos alturas es", 0.21 * altura2)
+
+
+
+
+
+
+
+# Convierte a integer (entero)
+# Convierte float (altura) a String = str(altura)
+'''
+if (int(edad) < 0):
+	print("La edad no puede ser negativa")
+elif(int(edad) == 18):
+		print("Eres mayor de edad")
+else:
+	print("El nombre de la persona es: ", nombrePersona, "y su edad es: ", edad)
+'''
+
+
+
+'''
+if (condicion):
+	codigo1
+else:
+	codigo2
+'''

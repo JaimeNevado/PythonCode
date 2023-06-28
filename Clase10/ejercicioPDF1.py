9d71c1dfad01576a059f4259cb085f5a189cdf3a
Jaime Nevado Farfán <djamperiio@gmail.com>
2023-06-28 09:58:14 +0200

    Código

--- Clase10/ejercicioPDF1.py
+++ Clase10/ejercicioPDF1.py
@@ -33,15 +33,15 @@ def asignar_colores():
         color = random.choice(colores)
         usuario['Color'] = color
         colores.remove(color)
-        print(f"Se asignó el color '{color}' al usuario {usuario['Nombre']}.")
+        print("Se asignó el color ", color, "al usuario ", usuario['Nombre'])
 
 def eliminar_usuario():
     while True:
         try:
-            indice = int(input("Ingrese el índice del usuario a eliminar (0-{}): ".format(len(usuarios)-1)))
+            indice = int(input("Ingrese el índice del usuario a eliminar 0 -", len(usuarios)-1))
             if 0 <= indice < len(usuarios):
                 usuario_eliminado = usuarios.pop(indice)
-                print(f"El usuario {usuario_eliminado['Nombre']} fue eliminado.")
+                print("El usuario", usuario_eliminado['Nombre'], "fue eliminado.")
                 break
             else:
                 print("Índice inválido. Inténtelo nuevamente.")
@@ -49,6 +49,7 @@ def eliminar_usuario():
             print("Valor inválido. Inténtelo nuevamente.")
 
 def mostrar_usuarios_ordenados():
+    # Explicar el .sort en listas y el sorted en diccionarios
     usuarios_ordenados = sorted(usuarios, key=lambda x: x['Nombre'])
     print("Usuarios ordenados alfabéticamente:")
     for usuario in usuarios_ordenados:
@@ -60,7 +61,7 @@ def mostrar_nombres_usuarios():
         print(usuario['Nombre'])
 
 def salir():
-    print("¡Hasta luego!")
+    print("¡Chao pescao!")
     exit()
 
 def mostrar_menu():
@@ -74,7 +75,7 @@ def mostrar_menu():
     print("7. Mostrar usuarios solo con el Nombre")
     print("8. Salir")
 
-acciones = {
+'''
     '1': agregar_color,
     '2': mostrar_colores,
     '3': ordenar_colores,
@@ -83,13 +84,27 @@ acciones = {
     '6': mostrar_usuarios_ordenados,
     '7': mostrar_nombres_usuarios,
     '8': salir
-}
+'''
 
 while True:
     mostrar_menu()
-    opcion = input("Ingrese el número de la acción que desea realizar: ")
+    opcion = int(input("Ingrese el número de la acción que desea realizar: "))
 
-    if opcion in acciones:
-        acciones[opcion]()
+    if opcion == 1:
+        agregar_color()
+    elif opcion == 2:
+        mostrar_colores()
+    elif opcion == 3:
+        ordenar_colores()
+    elif opcion == 4:
+        asignar_colores()
+    elif opcion == 5:
+        eliminar_usuario()
+    elif opcion == 6:
+        mostrar_usuarios_ordenados()
+    elif opcion == 7:
+        mostrar_nombres_usuarios()
+    elif opcion == 8:
+        salir()
     else:
-        print("O");
+        print("Error en la opcion: ");
---
+++ Clase10/explicacion.py
@@ -0,0 +1,8 @@
+colores = ["amarillo", 'azul', 'verde', 'rojo']
+
+colores.append("blanco")
+# ["amarillo", 'azul', 'verde', 'rojo', 'blanco']
+colores.append("violeta")
+
+for color in colores:
+    print(color)
